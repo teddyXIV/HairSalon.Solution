d789f53051671801e35518c43e4f36b01f82e002
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2024-02-02 14:40:29 -0800

    add stylist delete functionality

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -52,4 +52,19 @@ public class StylistsController : Controller
         _db.SaveChanges();
         return RedirectToAction("Index");
     }
+
+    public ActionResult Delete(int id)
+    {
+        Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+        return View(thisStylist);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+        Stylist thisStylist = _db.Stylists.FirstOrDefault(Stylist => Stylist.StylistId == id);
+        _db.Stylists.Remove(thisStylist);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
 }
